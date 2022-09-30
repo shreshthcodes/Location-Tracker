375fbb5980cb35dd5cf976ef2ac426316b79b445
Shreshth Sharma <shreshthsharma.contact@gmail.com>
2022-10-01 00:29:41 +0530

    style changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import ParticlesBg from 'particles-bg'
 import Gps from './Components/Gps';
 import Heading from './Components/Heading';
 const App=()=> {
   return (
+    <>
     <div className='container'>
     <Heading/>
     <Gps/>
     </div>
+    <ParticlesBg type="circle" bg={true} />
+    </>
   );
 }
 export default App;
